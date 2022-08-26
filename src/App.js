ac32946a62ada3e6749967a22cf8ed23e560911e
matheusdearaujo <matheusouzadearaujo@gmail.com>
2022-08-25 22:34:04 -0300

    Remove comments

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import { Navbar } from "./components/Navbar/Navbar.js";
 import { Main } from "./components/Main/Main.js";
 
-// import app from "./app.css" assert { type: "css" };
-
 export const App = () => {
   const App = document.createElement("div");
 
   App.setAttribute("class", "App");
-  // document.adoptedStyleSheets = [app];
 
   App.append(Navbar());
   App.append(Main());
